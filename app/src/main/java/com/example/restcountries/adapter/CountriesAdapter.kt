f1b25f49cbe696e31e61b4685a6b2396997f07df
FireLeha <zegger1@gmail.com>
2023-01-18 12:25:28 +0300

    Переделал на coil svg, теперь показывает флаги

--- app/src/main/java/com/example/restcountries/adapter/CountriesAdapter.kt
+++ app/src/main/java/com/example/restcountries/adapter/CountriesAdapter.kt
@@ -3,12 +3,16 @@ package com.example.restcountries.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 import com.example.restcountries.R
 import com.example.restcountries.data.CountryModel
 
-class CountriesAdapter: RecyclerView.Adapter<CountriesAdapter.MainViewHolder>() {
+class CountriesAdapter : RecyclerView.Adapter<CountriesAdapter.MainViewHolder>() {
 
     private var countriesData: List<CountryModel> = listOf()
 
@@ -17,10 +21,15 @@ class CountriesAdapter: RecyclerView.Adapter<CountriesAdapter.MainViewHolder>() 
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountriesAdapter.MainViewHolder {
-        return MainViewHolder(LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.fragment_countries_list, parent, false))
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): CountriesAdapter.MainViewHolder {
+        return MainViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.fragment_countries_list, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: CountriesAdapter.MainViewHolder, position: Int) {
@@ -32,12 +41,25 @@ class CountriesAdapter: RecyclerView.Adapter<CountriesAdapter.MainViewHolder>() 
     }
 
 
-
-    inner class MainViewHolder(view: View): RecyclerView.ViewHolder(view){
-        fun bind(data: CountryModel){
-            with(itemView){
-                findViewById<TextView>(R.id.tvName).text =data.name
+    inner class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(data: CountryModel) {
+            with(itemView) {
+                findViewById<TextView>(R.id.tvName).text = data.name
+                findViewById<ImageView>(R.id.flagImage).loadUrl(data.flag)
             }
+        }
+
+        private fun ImageView.loadUrl(url: String) {
+            val imageLoader = ImageLoader.Builder(this.context)
+                .components{add(SvgDecoder.Factory())}
+                .build()
+
+            val request = ImageRequest.Builder(this.context)
+                .data(url)
+                .target(this)
+                .build()
+
+            imageLoader.enqueue(request)
         }
     }
 }
--- app/src/main/java/com/example/restcountries/data/CountryModel.kt
+++ app/src/main/java/com/example/restcountries/data/CountryModel.kt
@@ -1,12 +1,12 @@
 package com.example.restcountries.data
 
 data class CountryModel(
-    val name: String?,
-    val flag: String?,
-    val region: String?,
-    val capital: String?,
+    val name: String,
+    val flag: String,
+    val region: String,
+    val capital: String,
 //    val currencies: List<Currency>,
-    val timezones: List<String>?
+    val timezones: List<String>
 )
 
 //data class Currency(
